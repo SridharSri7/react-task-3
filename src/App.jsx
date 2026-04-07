ed9cec135c8d6fe3daf7367dc0cf1785dc896310
Sridhar <usridhar@thestackly.com>
2026-04-07 15:45:54 +0530

    task-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ConditionalRendering from './components/ConditionalRendering.jsx'
 import ListRendering from './components/ListRendering.jsx'
 import Combined from './components/Combined.jsx'
+import UseState from './components/UseState.jsx'
 
 function App() {
 
 
   return (
     <>
+
+      <UseState/>
+      <hr />
       <ConditionalRendering />
       <hr />
       <ListRendering />
---
+++ src/components/UseState.jsx
@@ -0,0 +1,50 @@
+
+// Task-4
+
+import React, { useState } from 'react'
+
+const UseState = () => {
+    
+    const [user, setuser] = useState(false)
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+
+    const handleButton = () => {
+        if (!user){
+            if(email&&password){
+                setuser(true)
+            }else{
+                alert("enter details")
+            }
+        }else{
+            setemail("")
+            setpassword("")
+            setuser(false)
+        }
+    }
+  return (
+    <div>
+        
+       <h1> {user ? "Welcome to our app" : "Sign in page"}</h1>
+
+       { !user && (
+        <>
+           <label htmlFor="email">Email:</label>
+           <input type="email" id="email" value={email} onChange={(e) => setemail(e.target.value)}  />
+           <label htmlFor="password">Password:</label>
+           <input type="password" id="password" value={password} onChange={(e) => setpassword(e.target.value)}  />
+
+        </>
+       )}
+
+
+      <button onClick={handleButton}>Sign {user? "Out" : "In"}</button>
+      
+    </div>
+  )
+}
+
+export default UseState
+
+
+
